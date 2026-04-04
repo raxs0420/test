1ce477b2233a8ecc2dd91a56d2254251fddfc245
raxs0420 <oskar6derda@gmail.com>
2026-04-04 17:35:14 +0200

    Update new ui test test.lua

--- library new/new ui test test.lua
+++ library new/new ui test test.lua
@@ -1629,7 +1629,7 @@ local function start_auto_uber()
                             "Troops",
                             "Abilities",
                             "Activate",
-                            { Troop = tower, Name = "Ubercharge", Data = {} }
+                            { Troop = medic, Name = "Ubercharge", Data = {} }
                         )
                         
                         task.wait(0.5) 
