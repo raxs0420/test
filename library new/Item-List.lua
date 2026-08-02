51cb9e1784a2541502da6eeb8f244c66d9f7d7a4
raxs0420 <oskar6derda@gmail.com>
2026-08-02 06:12:16 +0200

    Create Item-List.lua

---
+++ library new/Item-List.lua
@@ -0,0 +1,41 @@
+return {
+    -- Chests
+    ["rbxassetid://71674076269696"] = "Scuba Ops Crate",
+    
+    -- Consumables
+    ["rbxassetid://17447507910"] = "Timescale Ticket(s)",
+    ["rbxassetid://18493073533"] = "Spin Ticket(s)",
+    ["rbxassetid://98556517737333"] = "Low Grade Consumable Crate(s)",
+    ["rbxassetid://90205718290985"] = "Mid Grade Consumable Crate(s)",
+    ["rbxassetid://18443277591"] = "High Grade Consumable Crate(s)",
+    ["rbxassetid://17438486690"] = "Range Flag(s)",
+    ["rbxassetid://17438486138"] = "Damage Flag(s)",
+    ["rbxassetid://17438487774"] = "Cooldown Flag(s)",
+    ["rbxassetid://100201829226448"] = "Ducky Squad(s)",
+    ["rbxassetid://17429537022"] = "Blizzard(s)",
+    ["rbxassetid://17448596749"] = "Napalm Strike(s)",
+    ["rbxassetid://17429548305"] = "Supply Drop(s)",
+    ["rbxassetid://136180382135048"] = "Santa Radio(s)",
+    ["rbxassetid://115421293343588"] = "Easter Egg(s)",
+    ["rbxassetid://95120437798143"] = "Molten Monster(s)",
+    ["rbxassetid://132155797622156"] = "Christmas Tree(s)",
+    ["rbxassetid://124065875200929"] = "Fruit Cake(s)",
+    ["rbxassetid://17429541513"] = "Barricade(s)",
+    ["rbxassetid://110415073436604"] = "Holy Hand Grenade(s)",
+    ["rbxassetid://17429533728"] = "Frag Grenade(s)",
+    ["rbxassetid://17437703262"] = "Molotov(s)",
+    ["rbxassetid://139414922355803"] = "Present Clusters(s)",
+    
+    -- Main
+    ["rbxassetid://6794340240"] = "XP",
+    ["rbxassetid://128907328829271"] = "Coins",
+    ["rbxassetid://108777382125602"] = "Gems",
+    
+    -- Evolved XP
+    ["rbxassetid://16742136768"] = "Scout XP",
+    ["rbxassetid://16742109340"] = "Minigunner XP",
+    ["rbxassetid://16742038000"] = "Crook Boss XP",
+    ["rbxassetid://94394209436954"] = "Operator XP",
+    ["rbxassetid://131022816552952"] = "Juggernaut XP",
+    ["rbxassetid://74602401810472"] = "Kingpin XP",
+}
